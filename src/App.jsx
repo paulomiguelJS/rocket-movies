eb59c1d8fe01eaa10f01eba12e9110eea5d44574
paulomigueljs <paulomiguelblues@gmail.com>
2022-02-14 19:46:05 -0300

    folders as services, and styles created and linear background applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <h1>Rocket-movie</h1></>
-  )
+    <div className="container">
+      <h1>Rocket-movie</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./main.css";
+import "./styles/global.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
