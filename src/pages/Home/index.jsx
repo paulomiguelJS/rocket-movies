15fdbf9991b84194a2eec780fdfd5a08a5189e7b
paulomigueljs <paulomiguelblues@gmail.com>
2022-02-15 14:33:27 -0300

    Adding img on home and styled started

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import Logo  from "../../components/Logo";
 import  Movies  from "../../components/Movies";
+import shuffle from '../../assets/shuffle.svg'
+import { mainContent } from "./styles";
 
 const Home = () => {
   return (
-    <>
-      <Logo />
+    <div>
+    <img src={shuffle} alt="Suffle" />
       <Movies />
-    </>
+    </div>
   );
 };
 
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const mainContent = styled.div`
+  img {
+    background-color: red;
+    border: 1px solid red;
+  }
+`;
