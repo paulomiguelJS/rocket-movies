f650b857d54d62de51dbf4e3267722d51ebd00cd
paulomigueljs <paulomiguelblues@gmail.com>
2022-02-17 17:17:51 -0300

    initializing fetch

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Movies from "../../components/Movies";
 import shuffle from "../../assets/shuffle.svg";
 import { MainContent } from "./styles";
@@ -6,6 +6,8 @@ import Button from "../../components/Button";
 import { api } from "../../services/api";
 
 const Home = () => {
+
+ 
 
   useEffect(() => {
     loadMovie()
@@ -16,23 +18,18 @@ const Home = () => {
     let data = await api.getMovie()
   }
 
+ 
+
   return (
     <>
       <MainContent>
         <img src={shuffle} alt="Suffle" />
         <h1>Nao sabe o que assitir?</h1>
         <Movies
-          title="A volta dos que nao foram"
-          description="Em Nova York Peter Venkman, Ray Stantz e Egon Spengler são três
-cientistas do departamento de psicologia da Columbia University, que
-se dedicam ao estudo de casos paranormais. Quando a subvenção
-termina eles são despedidos e Venkman sugere que abram um negócio
-próprio, a exterminadora de fantasmas Ghostbusters. Inicialmente
-eles só têm despesas e nenhum cliente, mas eis que surge Dana
-Barrett, uma violoncelista que teve uma experiência assustadora em
-seu apartamento."
+          title=""
+          description=""
         />
-        <Button title="Encontrar Filme"/>
+        <Button  title="Encontrar Filme"/>
         <span>
           Clique em "Encontrar filme" que  traremos informações <br /> de algum filme
           para você assistir hoje.
--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,15 @@
-const BASE = "https://api.themoviedb.org/3";
-const KEY = "api_key=559e0716959e653ce25f7d162bdb410b"
-
-console.log(BASE)
+const BASE = "https://api.themoviedb.org/3/movie/";
+const movieId = Math.floor(Math.random() * 1000 + 1);
+const KEY = "559e0716959e653ce25f7d162bdb410b";
+console.log(movieId);
 
 export const api = {
-    
-         getMovie: async () => {
-            let response = await fetch(`${BASE}${KEY}`)
-            let data = await response.json()
-            console.log(data)
-            return data
-    }
-}
-
+  getMovie: async () => {
+    let response = await fetch(`${BASE}${movieId}?api_key=${KEY}`);
+    let data = await response.json();
+    console.log(movieId)
+    return data;
+  },
+};
 
 /*https://api.themoviedb.org/3/movie/76341?api_key=559e0716959e653ce25f7d162bdb410b*/
-
-
-
